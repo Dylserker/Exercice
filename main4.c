0dc642c5e588707d8c63d1f7e66434e6ab8fb2bd
dylserker <dylserker@msi.home>
2024-09-26 18:17:59 +0200

    exo-5

---
+++ main4.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{ 
+    int number_1 = 6;
+    int number_2 = 7;
+    int resultat = number_1 * number_2;
+    
+    printf("%d x %d = %d\n", number_1, number_2, resultat); 
+    exit(0);
+}
