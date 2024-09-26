0d19d78af494ff22a9c47c22f7754fd13b8e1206
dylserker <dylserker@msi.home>
2024-09-26 18:24:30 +0200

    exo6

---
+++ main5.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{ 
+    double number_1 = 12; 
+    double number_2 = 7;
+    double result = (number_1 + number_2) / 2;
+    
+    printf("La moyenne de 12 et 7 est égale à %.2f\n", result);
+    exit(0);
+}
